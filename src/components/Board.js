c9c619275b994d3a5d4fe92ff9bc1d82d9a3724c
trand <127272317+DUCANHTRA@users.noreply.github.com>
2025-06-11 17:37:59 +0700

    Implement List Drag and Drop functionalities in Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -9,31 +9,28 @@ import CloseIcon from '@mui/icons-material/Close';
 import List from '../components/List';
 
 const Board = () => {
+
+    const lists = useSelector((state) => state.board.lists);
+    const dispatch = useDispatch();
+
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [modalText, setModalText] = useState('');
 
-    const dispatch = useDispatch();
-    const lists = useSelector((state) => state.board.lists);
-
     const openModal = () => setIsModalOpen(true);
-
     const closeModal = () => {
         setModalText('');
         setIsModalOpen(false);
-    };
+    }
 
     const addList = async () => {
         if (modalText === "") {
             window.alert("Please enter a list name");
             return;
         }
-
-        dispatch(createNewListReducer({ listTitle: modalText }));
-
+        dispatch(createNewListReducer({listTitle: modalText}));
         setModalText('');
         closeModal();
-    };
-
+    }
 
     const modalStyles = {
         content: {
@@ -55,61 +52,65 @@ const Board = () => {
         },
     };
 
-
     return (
         <div className="board-container">
-            <div className="board">
-            {lists.length !== 0 && (
-                    <>
-                        {lists.map((list) => (
-                            <List 
-                                key={list.listId}
-                                list={list}
-                            />
-                        ))}
-                    </>
-                )}
-            </div>
-
+            <DragDropContext onDragStart={() => console.log('Drag Started')} onDragEnd={async (results) => dispatch(dragReducer({results: results}))}>
+                <Droppable droppableId="boardDroppable" type="listDrag" direction="horizontal">
+                    {(provided) => (
+                        <div 
+                            className="board"
+                            ref={provided.innerRef}
+                            {...provided.droppableProps}
+                        >
+                            {lists.length !== 0 && (
+                                <>
+                                    {lists.map((list, index) => (
+                                        <Draggable draggableId={list.listId} key={list.listId} index={index}>
+                                            {(provided) => (
+                                                <div
+                                                    {...provided.dragHandleProps}
+                                                    {...provided.draggableProps}
+                                                    ref={provided.innerRef}
+                                                    data-testid='draggable-list'
+                                                >
+                                                    <List 
+                                                        list={list}
+                                                    />
+                                                </div>
+                                            )}
+                                        </Draggable>
+                                    ))}
+                                </>
+                            )}
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </DragDropContext>
             <div className="board-button">
                 <button data-testid='list-add' className="list-add" onClick={() => openModal()}>
-                    <AddIcon style={{ fontSize: "2em", color: "black" }} />
-                    <h2 className="list-add-text">Create a new list</h2>
+                    <AddIcon style={{ fontSize: "2em", color: "black" }}/>
+                    <h2 className="list-add-text"> Create a new list </h2>
                 </button>
             </div>
-            
-
-            <Modal
-                isOpen={isModalOpen}
-                ariaHideApp={false}
-                onRequestClose={closeModal}
-                style={modalStyles}
-                testId='list-add-modal'
-            >
-
-                <CloseIcon
-                    data-testid='list-add-modal-close'
-                    style={{ fontSize: "40px", color: "red", position: "absolute", top: 0, right: 0, padding: "10px" }}
-                    onClick={() => closeModal()}
-                />
-
-                <input
-                    type="text"
-                    value={modalText}
-                    onChange={(event) => setModalText(event.target.value)}
-                    placeholder="Enter list name"
-                    className="modal-input"
-                    data-testid='list-add-input'
-                />
-
-                <button
-                    data-testid='create-list-button'
-                    className="modal-button"
-                    onClick={() => addList()}
+            <Modal 
+                    isOpen={isModalOpen} 
+                    ariaHideApp={false} 
+                    onRequestClose={() => closeModal()}
+                    style={modalStyles}
+                    testId='list-add-modal'
                 >
-                    Create list
-                </button>
-            </Modal>  
+                    <CloseIcon data-testid='list-add-modal-close' style={{ fontSize: "40px", color: "red", position: "absolute", top: 0, right: 0, padding: "10px" }} onClick={() => closeModal()}/>
+                    <input
+                        type="text"
+                        value={modalText}
+                        onChange={(event) => setModalText(event.target.value)}
+                        placeholder="Enter list name"
+                        className="modal-input"
+                        data-testid='list-add-input'
+                    />
+                    <button data-testid='create-list-button' className="modal-button" onClick={() => addList()}> Create list </button>
+            </Modal>
         </div>
     );
 }
